87473ff12fc41b9fcd2a3d6f433906def34ab24b
mauricio-melo <mauriciomelo.contato@outlook.com>
2022-09-06 15:51:42 -0300

    Criação da task do gradle que sera chamada pela pipeline

--- auth-persistence/build.gradle.kts
+++ auth-persistence/build.gradle.kts
@@ -1,26 +1,31 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
+import org.flywaydb.gradle.task.FlywayMigrateTask
 
 plugins {
-	id("org.flywaydb.flyway") version "8.2.0"
+    id("org.flywaydb.flyway") version "8.2.0"
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	runtimeOnly("mysql:mysql-connector-java")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    runtimeOnly("mysql:mysql-connector-java")
 }
 
-flyway {
-	url = "jdbc:mysql://localhost:3306/auth_dev?allowPublicKeyRetrieval=true&createDatabaseIfNotExist=true&useSSL=false&useTimezone=true&serverTimezone=UTC"
-	user = "root"
-	password = "12345678"
-	locations = arrayOf("filesystem:src/main/resources/flyway")
+tasks.register("database_flyway", FlywayMigrateTask::class) {
+    if (!project.hasProperty("url")) throw StopExecutionException("Args url is mandatory")
+    if (!project.hasProperty("user")) throw StopExecutionException("Args user is mandatory")
+    if (!project.hasProperty("password")) throw StopExecutionException("Args password is mandatory")
+    if (!project.hasProperty("locations")) throw StopExecutionException("Args locations is mandatory")
+    url = project.property("url").toString()
+    user = project.property("user").toString()
+    password = project.property("password").toString()
+    locations = arrayOf("filesystem:" + project.property("locations").toString())
 }
 
 tasks.withType<BootJar> {
-	enabled = false
+    enabled = false
 }
 
 tasks.jar {
-	enabled = true
+    enabled = true
 }
